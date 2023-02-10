589ebc2b3369646e52fb49af557a8b0470d2f04c
Aaron Barbosa <54488243+AaronBarbosa12@users.noreply.github.com>
2023-02-09 21:28:24 -0600

    Add files via upload

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -4,10 +4,12 @@ from translator import english_to_french, french_to_english
 class TestTranslator(unittest.TestCase):
     def test_englishToFrench(self):
         self.assertEqual(english_to_french("Hello"), "Bonjour")
+        self.assertNotEqual(english_to_french(None), "Bonjour")
         self.assertIsNone(english_to_french(None))
 
     def test_frenchToEnglish(self):
         self.assertEqual(french_to_english("Bonjour"), "Hello")
+        self.assertNotEqual(french_to_english(None), "Hello")
         self.assertIsNone(french_to_english(None))
 
 if __name__ == "__main__":
